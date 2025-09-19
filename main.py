ea846704d7bab6f3130ee47fbc09c2c7c14d59e9
Kooi <liangkooi2012@gmail.com>
2025-09-19 14:12:25 +0800

    Add files via upload
    Add in LLM part

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
+"""
+Author: Yap
+
+Descriptions:
+1. Set your API key in the local environment.
+    - If you are using VSCode, under the terminal, write 
+"""
+
+
+
 from SchemaDataExtractor import DatabaseExtractor
+from LLMAgent import LLMKGAgent
+import json
+import os
+
 
 extractor = DatabaseExtractor()
-schema = extractor.extract_schema("flight_company.sqlite")
+schema = extractor.extract_schema("C:/Users/ewa_c/lkyap/UWA - Master DS Course Units/S2-2025/Project/spider_data/spider_data/database/flight_company/flight_company.sqlite")
 print(schema)
 print("\n")
 
-data = extractor.extract_data("flight_company/flight_company.sqlite")
-print(data)
-print("\n")
+# Initialize agent and model
+LLMAgent = LLMKGAgent(model="gpt-5-mini")
 
-schema_data = extractor.extract_schema_data("flight_company.sqlite")
-print(schema_data)
-print("\n")
+# Discover entities
+entities = LLMAgent.discover_entities(schema)
+print("Entities as below:\n")
+print(json.dumps(entities,indent=2))
 
-print(schema_data["schema"])
-print("\n")
+# Discover relationship
+relationship = LLMAgent.discover_relationship(schema,entities)
+print(f"Relationship as below:\n {json.dumps(relationship, indent=2)}")
 
-print(schema_data["data"])
 
 
-# Export to JSON
-extractor.export_to_json(schema_data,"airport_SchemaData")
