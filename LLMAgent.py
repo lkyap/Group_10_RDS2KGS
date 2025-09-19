a14b37721d6f82765b136e5e5e42ffb9ff7bc17a
Kooi <liangkooi2012@gmail.com>
2025-09-19 14:17:15 +0800

    Add files via upload
    Uploaded LLM Agent

---
+++ LLMAgent.py
@@ -0,0 +1,37 @@
+import json
+from openai import OpenAI
+from LLMPrompt import entity_discovery_prompt, relationship_discovery_prompt
+
+class LLMKGAgent:
+    def __init__(self, model = "gpt-5-mini"):
+        self.client = OpenAI()
+        self.model = model
+
+    def discover_entities(self, schema: dict):
+        # Use dictionary to run entity discovery
+        schema_str = json.dumps(schema, indent=2)
+        response = self.client.chat.completions.create(
+            model = self.model,
+            messages = [
+                {"role":"system","content": entity_discovery_prompt},
+                {"role":"user","content": f"Database schema provided:\n {schema_str}"}
+            ],
+            response_format={"type":"json_object"} # Return JSON format
+        )
+        return json.loads(response.choices[0].message.content)
+    
+    def discover_relationship(self, schema:dict,entity_config: list[dict]):
+        # Discover relationship with schema and entity configuration
+        schema_str = json.dumps(schema,indent=2)
+        entity_str = json.dumps(entity_config, indent=2)
+
+        response = self.client.chat.completions.create(
+            model = self.model,
+            messages = [
+                {"role":"system","content": relationship_discovery_prompt},
+                {"role":"user","content":f"Database schema:\n{schema_str}.\n Entity configuration:\n{entity_str}"}
+            ],
+            response_format = {"type":"json_object"} # get the response in json format
+        )
+        return json.loads(response.choices[0].message.content)
+    
