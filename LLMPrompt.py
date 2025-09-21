6a3a3a04585b79e185f0ffa73d67dc8b23235663
Kooi <liangkooi2012@gmail.com>
2025-09-21 15:19:52 +0800

    Add files via upload
    Added in New LLM Prompts for Full Knowledge Graph to be created by LLM

--- LLMPrompt.py
+++ LLMPrompt.py
@@ -1,3 +1,14 @@
+"""
+Author: Yap
+
+Descriptions:
+- This script only contains the LLM prompts for system role.
+- Tune the prompt if needed for better performance.
+- Check the main.py to see how it works out.
+
+"""
+
+
 entity_discovery_prompt = """
 You are expertise in knowledge graph schema design and relational database design.
 You have extensive experience in extracting entities and their properties from structured data to design efficient and meaningful knowledge graphs. 
@@ -17,6 +28,35 @@ Output: Return the result strictly in JSON format as an array where each object 
 3. is_key_property
 4. source_table
 5. source_column
+
+Example:
+
+{
+  "entities": [
+    {
+      "entity": "Airport",
+      "property": "id",
+      "is_key_property": true,
+      "source_table": "airport",
+      "source_column": "id"
+    },
+    {
+      "entity": "Airport",
+      "property": "City",
+      "is_key_property": false,
+      "source_table": "airport",
+      "source_column": "City"
+    },
+    {
+      "entity": "Airport",
+      "property": "Country",
+      "is_key_property": false,
+      "source_table": "airport",
+      "source_column": "Country"
+    }
+    ]
+}
+
 """
 
 relationship_discovery_prompt = """
@@ -35,6 +75,8 @@ Ways to discover relationships:
 5. If key properties of connecting entities exist in different tables, check if common column can be used for joining and establish relationship. 
 6. Ensure correct relationship direction such as "player Plays match", "airport LOCATED_AT perth", "student ENROLLED units".
 7. The relationships identified should be meaningful and useful for users to get answer query
+8. Avoid duplicate relationships such as student TEACHED_BY teacher, and teacher TEACHED student
+
 
 Output: Return the result strictly in JSON format as an array where each object has:
 1. Source_Entity
@@ -48,4 +90,124 @@ Output: Return the result strictly in JSON format as an array where each object 
 9. Target_Column
 10. Joining_Condition (if applicable)
 
+Example:
+{ "relationships" :[{
+"Source_Entity": "Flight",
+"Relationship": "OPERATED_BY",
+"Target_Entity": "Operate_Company",
+"Source_Property": "company_id",
+"Target_Property": "id",
+"Source_Table": "flight",
+"Target_Table": "operate_company",
+"Source_Column": "company_id",
+"Target_Column": "id",
+"Joining_Condition": "flight.company_id = operate_company.id"},
+{
+"Source_Entity": "Flight",
+"Relationship": "DEPARTS_FROM",
+"Target_Entity": "Airport",
+"Source_Property": "airport_id",
+"Target_Property": "id",
+"Source_Table": "flight",
+"Target_Table": "airport",
+"Source_Column": "airport_id",
+"Target_Column": "id",
+"Joining_Condition": "flight.airport_id = airport.id"
+}
+]}
+
 """
+
+ 
+graph_entity_prompt = """
+You are an expert in knowledge graph design and relational database modeling. 
+You are provided with the schema of a relational database. 
+Your task is to discover a complete knowledge graph representation from it
+
+You will be given the relational database schema with 2 keys, "tables" and "foreign_keys".
+In the tables, you will have multiple tables. In each table, you will have multiple columns. 
+The foreign_keys will have the information about the parent table and from table and their respective columns name
+
+Example as below:
+{
+"tables":{table1:{columns:[column_1,column_2,...],"primary_key":[primary_keys]}, {table2:{columns:[column_1,column_2,....],"primary_key":[primary_keys]}},....,},
+"foreign_keys":[{"from_table":from_table_name,"parent_table":parent_table_name,"from_column":from_column_name,"parent_column":parent_column_name},{"from_table":from_table_name,"parent_table":parent_table_name,"from_column":from_column_name,"parent_column":parent_column_name},....]
+}
+
+
+Instructions:
+1. Discover entities / nodes in graph:
+Your task is to identify meaningful entities such as Airport, Flight, Company from the given schema.
+For each identified entity, you will identify key property and list all relevant properties which you can extract from the columns name.
+You will NOT duplicate properties across multiple entities.
+Consider to merge properties if multiple tables states the same property or properties.
+You will use the exact column name as properties. Do not rename them. 
+
+2. Discover relationships / edges in graph:
+Your task is to identify meaningful and non-generic relationships between entities such as flight DEPARTS_FROM airport, player PLAYS match, teacher TEACHES student.
+You can derive the relationship explicitly from foreign key or multiple foreign keys in the schema given which is/are join tables.  
+Make sure the direction of the relationship is sensible such as flight DEPARTS_FROM airport, company OPERATES airline, doctor PROVIDE prescription
+You should make sure the relationship is unique and prevent duplicated relationship such as teacher TEACHES student and student TEACHED_BY teacher are duplicated
+
+Output format required:
+
+Return a single JSON object that contains 2 keys:
+
+1. "Nodes": An array of entities. Each object must contain the following
+"id"(string): Entity Name (Example: Flight,Company, Employee, Employer, University)
+"Properties" (List of String): Usually is columns in relational database / property names
+"key" (String): Primary key of relational database
+Example of nodes:
+"id": Employee, "Properties": [employee_id, employee_name, Birth of Date, start_date], "key": employee_id
+"id": Company, "Properties":[company_id, company_name], "key": company_id
+
+
+2. "Edges": An array of relationships. Each object must contain the following:
+"source" (String): Source Entity Name such as flight
+"target" (String): Target Entity Name such as airport
+"relationship" (String): Relationship name/ type of relationship such as departs_from
+"source_column" (String): column name in source table. Do not rename the column name, use the column name in source table
+"target_column" (String): Column name in target table. Do not rename the column name, use the column name in target table 
+Example of edges: 
+source: Flight, target: Airport, relationship: Departs_from, source_column: airport_id, target_column: id
+source: Student, target: Study_Units, relationship: Enrolled, source_column: unit_id, target_column: id
+
+Output Example:
+
+{
+  "nodes": [
+    {
+      "id": "Airport",
+      "properties": ["id", "City", "Country", "IATA", "ICAO", "name"],
+      "key": "id"
+    },
+    {
+      "id": "Flight",
+      "properties": ["id", "Vehicle_Flight_number", "Date", "Pilot", "Velocity", "Altitude", "airport_id", "company_id"],
+      "key": "id"
+    }
+  ],
+  "edges": [
+    {
+      "source": "Flight",
+      "target": "Airport",
+      "relationship": "DEPARTS_FROM",
+      "source_column": "airport_id",
+      "target_column": "id"
+    },
+    {
+      "source": "Flight",
+      "target": "Operate_Company",
+      "relationship": "OPERATED_BY",
+      "source_column": "company_id",
+      "target_column": "id"
+    }
+  ]
+}
+
+
+"""
+
+
+
+
