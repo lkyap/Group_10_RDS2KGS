a663b0648a2fbb50ddada53be17c73fae291b56d
Kooi <liangkooi2012@gmail.com>
2025-10-02 15:36:11 +0800

    Remove unused code chunk
    Remove unused code chunk

--- LLMAgent.py
+++ LLMAgent.py
@@ -3,21 +3,27 @@ Author: Yap, Ashwin, Evanna
 
 Descriptions:
 1. Set up OpenAI model
-2. Use LLM to extract entity which represents nodes
-3. Use LLM to extract relationship based on entity extracted and database schema
+2. Pass in the extracted RDS schema and use LLM to generate the KGS schema
+
+Improvement plan:
+1. Use LLM to extract entity which represents nodes
+2. Use LLM to extract relationship based on entity extracted and database schema
+3. The extracted entity and relationship should be evaluated by expert before using it
+4. If the extracted entity and relationship are satisfied by user, generate the Knowledge Graph based on the entity and relationship
 
 """
 
 
 import json
 from openai import OpenAI
-from LLMPrompt import entity_discovery_prompt, relationship_discovery_prompt, graph_entity_prompt
+from LLMPrompt import entity_discovery_prompt, relationship_discovery_prompt, graph_entity_prompt,graph_entity_rds_prompt
 
 class LLMKGAgent:
-    def __init__(self, model = "gpt-5-mini"):
-        self.client = OpenAI()
+    def __init__(self,api_key ,model = "gpt-5-mini"):
+        self.client = OpenAI(api_key = api_key)
         self.model = model
 
+    # This is for entities discovery which is on the improvement plan
     def discover_entities(self, schema: dict):
         # Use dictionary to run entity discovery
         schema_str = json.dumps(schema, indent=2)
@@ -32,6 +38,7 @@ class LLMKGAgent:
         
         return json.loads(response.choices[0].message.content)
     
+    # This is for relationship discovery which is on the improvement plan
     def discover_relationship(self, schema:dict,entity_config: list[dict]):
         # Discover relationship with schema and entity configuration
         schema_str = json.dumps(schema,indent=2)
@@ -48,6 +55,7 @@ class LLMKGAgent:
         
         return json.loads(response.choices[0].message.content)
     
+    # Use RDS Schema to generate Knowledge Graph schema by using LLM
     def generate_kgs(self, schema:dict):
         schema_str = json.dumps(schema, indent=2)
         response = self.client.chat.completions.create(
@@ -59,3 +67,5 @@ class LLMKGAgent:
             response_format={"type":"json_object"}
         )
         return json.loads(response.choices[0].message.content)
+
+
